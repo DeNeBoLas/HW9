2ae758651a02a03a9634cc00e409b8ed4178e8ae
DeNeBoLas <svitlana-ilchenko@ukr.net>
2023-08-06 19:28:57 +0300

    fixed

--- bot.py
+++ bot.py
@@ -1,9 +1,10 @@
 ADDRESSBOOK = {}
 
+
 def input_error(wrap):
     def inner(
         *args,
-    ):  # передаемо аргументи в інер -> то буде наш аргумент data в функціях на яких весить декоратор
+    ):  # передаемо аргументи в інер -> то буде наш аргумент data в функціях на яких вuсить декоратор
         try:
             return wrap(*args)
         except IndexError:
@@ -15,18 +16,20 @@ def input_error(wrap):
 
     return inner
 
+
 @input_error
 def add_handler(data: list):
     print(data)
-    name = data[0].upper()
+    name = data[0].title()
     phone = data[1]
     ADDRESSBOOK[name] = phone
     return f"Contact {name} with {phone} was saved"
 
+
 @input_error
-def change_handler(*args): # зберігає в пам'яті новий номер телефону існуючого контакту
-    name = args[0].upper()
-    phone = args[1]
+def change_handler(data):  # зберігає в пам'яті новий номер телефону існуючого контакту
+    name = data[0].title()
+    phone = data[1]
     ADDRESSBOOK[name] = phone
     return f"Contact {name} with {phone} was changed"
 
@@ -38,46 +41,58 @@ def exit_handler(*args):
 def hello_handler(*args):
     return "How can I help you?"
 
-@input_error
-def phone(name:str): #ADDRESSBOOK.keys() 
-    return ADDRESSBOOK.values()
 
-def show_all(*args): #виводить всі збереженні контакти з номерами телефонів у консоль
-    for k, v in ADDRESSBOOK.items():
-        return f"Contact {k} with {v}"
-    
+@input_error
+def phone(data):  # тут після инпуту і парсера зайде список
+    name = data[0].title()
+    phone = ADDRESSBOOK.get(name)
+    if phone != None:
+        return f"{name}: number {phone}"
+    return "no name"
+
+
+def show_all(*args):  # виводить всі збереженні контакти з номерами телефонів у консоль
+    result = [
+        f"Name: {name} -->> number {phone}" for name, phone in ADDRESSBOOK.items()
+    ]
+    return "\n".join(result)
+
+
+def error(*args):
+    return "Unknown command"
+
 
 def command_parser(raw_str: str):
-    elements = raw_str.split()
+    elements = raw_str.split(" ")
     for key, value in COMMANDS.items():
         if elements[0].lower() in value:
             return key, elements[1:]
-        elif (' ' in elements[0]) in value: # тут потрібна перевірка на подвійні команди типу "good bye" зі своїм ретерном 
-            return key, elements[1:]      
-    return 'Unknown command' # тут ще один return коли такої команди не існуе..продумати то щоб цикл не впав а сказав невідома команда
-        
+        elif " ".join(elements[:2]).lower() in value:
+            return key, elements[2:]
+    return error, None
+
 
 COMMANDS = {
     add_handler: ["add", "+"],
     exit_handler: ["good bye", "close", "exit"],
     hello_handler: ["hello"],
     change_handler: ["change"],
-    phone:["phone"],
-    show_all: ["show all"]
+    phone: ["phone"],
+    show_all: ["show all"],
 }
 
 
 def main():
     while True:
-        user_input = input(">>>")  # add name 0987009090
+        user_input = input(">>>")
         if not user_input:
             continue
-        func,  data: list = command_parser(user_input)
+        func, data = command_parser(user_input)
         result = func(data)
         print(result)
         if func == exit_handler:
             break
-        print(ADDRESSBOOK)
+        # print(ADDRESSBOOK)
 
 
 if __name__ == "__main__":
